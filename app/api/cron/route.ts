d2bb896b512d35637b8bcf877889b9e99cc1d86e
Siddharth Gupta <123588855+Sid55a@users.noreply.github.com>
2024-02-21 04:37:41 +0530

    Update route.ts

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,8 +6,9 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import Product from "@/lib/actions/models/product.model";
 import { generateEmailBody, sendEmail } from "@/lib/nodeMailer";
 
-export const runtime = 'edge';
-export const maxDuration = 300;
+export const maxDuration = 10; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
  // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
