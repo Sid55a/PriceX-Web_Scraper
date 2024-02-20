29b12703011afe5675d6072103dc799655a8a1d8
Siddharth Gupta <siddharth55x@gmail.com>
2024-02-21 02:54:53 +0530

    first commit

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,10 +10,10 @@ export const maxDuration = 5; // This function can run for a maximum of 300 seco
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-export async function GET(request: Request) {
+export async function GET(req: Request) {
   try {
     connectToDB();
-
+ 
     const products = await Product.find({});
 
     if (!products) throw new Error("No product fetched");
